3e84203d96ea5f0250b99dd212afd0a9f2b75b75
Michael <nivmichael@gmail.com>
2018-03-01 17:26:46 +0200

    Going on - still intro

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@
 import {createStore} from 'redux';
 
 // Step 3 define reducers
-const reducer = function(state = 0, action) {
+const reducer = function(state = {books:[]}, action) {
     switch (action.type) {
-        case "INCREMENT":
-            return state + action.payload;
+        case "POST_BOOK":
+            let books = state.books.concat(action.payload);
+            return {books};
         break;
     }
 }
@@ -13,8 +14,38 @@ const reducer = function(state = 0, action) {
 const store = createStore(reducer);
 
 store.subscribe(function() {
-    console.log('current state is: ' + store.getState());
+    console.log('current state is: ', store.getState());
+
 })
 
 // Step 2 create and dispatch actions
-store.dispatch({type: "INCREMENT", payload: 1})
+store.dispatch({
+    type: "POST_BOOK",
+    payload: [
+        {
+            id: 1,
+            title: 'No. 1 book',
+            description: 'This is the desc of No.1',
+            price: '99.99$'
+
+        },
+        {
+            id: 2,
+            title: 'No. 2 book',
+            description: 'This is the desc of No.2',
+            price: '500.99$'
+        }]
+})
+
+// Dispatch a second action
+store.dispatch({
+    type: "POST_BOOK",
+    payload: [
+        {
+            id: 3,
+            title: 'No. 3 book',
+            description: 'This is the desc of No.3',
+            price: '99.99$'
+
+        }]
+})
