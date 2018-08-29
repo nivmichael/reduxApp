4d382c71b6b3ea917dddc7dea84c9390a4680f60
Michael <nivmichael@gmail.com>
2018-08-29 18:35:00 +0300

    Added Bootsrap

--- src/components/pages/booksList.js
+++ src/components/pages/booksList.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {getBooks} from '../../actions/booksActions';
+import {Grid, Col, Row, Button} from 'react-bootstrap';
 
 class BooksList extends React.Component {
     componentDidMount(){
@@ -16,14 +17,17 @@ class BooksList extends React.Component {
                     <h2>{booksArr.title}</h2>
                     <h2>{booksArr.description}</h2>
                     <h2>{booksArr.price}</h2>
+                    <Button bsStyle='primary'>Buy now</Button>
                 </div>
             )
         })
         return(
-            <div>
-                <h1>Hello Michael</h1>
-                {booksList}
-            </div>
+            <Grid>
+
+                <Row style={{marginTop: '15px'}}>
+                    {booksList}
+                </Row>
+            </Grid>
         )
 
     }
